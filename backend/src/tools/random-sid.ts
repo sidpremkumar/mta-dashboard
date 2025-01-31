c430b65beec00847602c1cf441fa65637d879ce4
Sid Premkumar <sid.premkumar@gmail.com>
2025-01-31 15:36:04 -0500

    random

--- backend/src/tools/random-sid.ts
+++ backend/src/tools/random-sid.ts
@@ -1,4 +1,6 @@
 import MTAClient from "../MTAClient/MTAClient";
+// @ts-ignore
+import brightness from 'brightness';
 
 const HEWES_ST = 100
 const METROPOLITAN_AVENUE = 629
@@ -7,5 +9,13 @@ async function testMTA() {
     const response = await MTAClient.getUpcomingTrainTimes({ stationId: METROPOLITAN_AVENUE, linePassed: 'L' })
 }
 
-testMTA();
+
+async function testbrightness() {
+
+    const level = await brightness.get();
+    console.log(level);
+    //=> 0.5
+
+}
+testbrightness();
 
